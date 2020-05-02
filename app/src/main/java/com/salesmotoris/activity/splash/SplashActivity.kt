0d2d40d8f155daeaa1fbf7eb21caae8f3ab1b076
Nalendra SH <nalendrasetya@gmail.com>
2020-05-02 21:50:27 +0700

    :hammer: splash full screen

--- app/src/main/java/com/salesmotoris/activity/splash/SplashActivity.kt
+++ app/src/main/java/com/salesmotoris/activity/splash/SplashActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
 import com.salesmotoris.R
 import com.salesmotoris.activity.home.HomeActivity
 import com.salesmotoris.activity.login.LoginActivity
@@ -15,6 +17,10 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
         setContentView(R.layout.activity_splash)
 
         Picasso.get().load(R.drawable.img_logo).into(imageview_splash_logo)
